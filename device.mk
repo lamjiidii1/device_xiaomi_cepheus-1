74af02adcfa8c2dd60a6662e1dd509a87936a719
root <root@Avaritia.dr0b51reesaenkmqiv2rwmzbfg.jx.internal.cloudapp.net>
2021-03-31 22:36:01 +0000

    try to fix fod

--- device.mk
+++ device.mk
@@ -183,7 +183,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += ro.incremental.enable=1
 
 # Enable blurs, hidden under dev option
-|PRODUCT_PRODUCT_PROPERTIES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
     persist.sys.sf.disable_blurs=1 \
     ro.sf.blurs_are_expensive=1
